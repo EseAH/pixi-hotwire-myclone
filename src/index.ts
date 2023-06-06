329d33de3d0be86c6d50b3c3078fcf8db98fa1e3
EseAH <ese.aquino@gmail.com>
2023-06-06 19:18:34 -0300

    resize game screen and margins

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,44 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("kisspng.png");
 
-clampy.anchor.set(0.5);
+window.addEventListener('resize', ()=>{
+	console.log('resized!');
+	const scaleX = window.innerHeight / app.screen.width
+	const scaleY = window.innerWidth / app.screen.height
+	const scale = Math.min(scaleX, scaleY)
 
-clampy.x = 300;
-clampy.y = 300;
+	const gameWidth = Math.round(app.screen.width * scale)
+	const gameHeight = Math.round(app.screen.height * scale)
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2)
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2)
+
+	app.view.style.width = gameWidth + 'px'
+	app.view.style.height = gameHeight + 'px'
+
+	app.view.style.marginLeft = marginHorizontal + 'px'
+	app.view.style.marginRight = marginHorizontal + 'px'
+
+	app.view.style.marginTop = marginVertical + 'px'
+	app.view.style.marginBottom = marginVertical + 'px'
+})
+window.dispatchEvent(new Event('resize'))
+
+Loader.shared.add({url: "./kiss.png", name: "myDino"})
+Loader.shared.add({url: "./clampy.png", name: "myClampy"});
+//const clampy: Sprite = Sprite.from("myDino");
+
+Loader.shared.onComplete.add(()=>{
+	const clampy: Sprite = Sprite.from("myClampy");
+	//clampy.anchor.set(0.5);
+	console.log("Hola mundo!", clampy.width, clampy.height);
+	clampy.x = 100;
+	clampy.y = 100;
+
+	clampy.scale.x = 0.5
+	clampy.scale.y = 0.5
 
 app.stage.addChild(clampy);
+})
+Loader.shared.load();
